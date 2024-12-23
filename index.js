3d78c48f3bf384d7b8cdbca25dedc943bf45228f
AbdulselamTaye <abdulselam.taye-ug@aau.edu.et>
2024-12-23 15:42:16 +0300

    adding js to the buttons

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-var randomNum1 = Math.floor(Math.random() * 6 + 1);
-var randomImg1 = "./assets/d" + randomNum1 + ".jpg";
-var randomNum2 = Math.floor(Math.random() * 6 + 1);
-var randomImg2 = "./assets/d" + randomNum2 + ".jpg";
+function rollDice() {
+  var randomNum1 = Math.floor(Math.random() * 6 + 1);
+  var randomImg1 = "./assets/d" + randomNum1 + ".jpg";
+  var randomNum2 = Math.floor(Math.random() * 6 + 1);
+  var randomImg2 = "./assets/d" + randomNum2 + ".jpg";
 
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImg1);
-var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", randomImg2);
-if (randomNum1 > randomNum2) {
-  document.querySelector("h1").innerHTML = "Player num 1 wins";
-} else if (randomNum2 > randomNum1) {
-  document.querySelector("h1").innerHTML = "Player num 2 wins";
-} else {
-  document.querySelector("h1").innerHTML = "draw!";
+  var image1 = document.querySelectorAll("img")[0];
+  image1.setAttribute("src", randomImg1);
+  var image2 = document.querySelectorAll("img")[1];
+  image2.setAttribute("src", randomImg2);
+  if (randomNum1 > randomNum2) {
+    document.querySelector("h1").innerHTML = "Player num 1 wins";
+  } else if (randomNum2 > randomNum1) {
+    document.querySelector("h1").innerHTML = "Player num 2 wins";
+  } else {
+    document.querySelector("h1").innerHTML = "draw!";
+  }
 }
-reloadGameButton.addEventListener("click", () => {
+// Function to reload the game
+function reloadGame() {
   location.reload();
-});
+}
+
+// Add event listeners to the buttons
+document.getElementById("rollDiceButton").addEventListener("click", rollDice);
+document.getElementById("reloadButton").addEventListener("click", reloadGame);
